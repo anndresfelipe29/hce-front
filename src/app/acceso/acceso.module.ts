2448507f381b1b2f6aa138aca57ca527d0faa3e2
Jose <jrortegar@correo.udistrital.edu.co>
2020-12-30 22:29:15 -0500

    Se agrega componente header

--- src/app/acceso/acceso.module.ts
+++ src/app/acceso/acceso.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AccesoRoutingModule } from './acceso-routing.module';
 import { LoginComponent } from './login/login.component';
 import { AccesoComponent } from './acceso.component';
+import { ComponentesCompartidosModule } from '../componentes-compartidos/componentes-compartidos.module';
 
 
 @NgModule({
   declarations: [LoginComponent, AccesoComponent],
   imports: [
     CommonModule,
-    AccesoRoutingModule
+    AccesoRoutingModule,
+    ComponentesCompartidosModule
   ]
 })
 export class AccesoModule { }
